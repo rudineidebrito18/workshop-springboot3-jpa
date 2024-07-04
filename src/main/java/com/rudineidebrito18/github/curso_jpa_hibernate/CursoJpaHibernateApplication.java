af2e1c1b1c160befa085eabb136edd0bf8f9d29e
Jose Rudinei de Brito <rudineidebrito18@gmail.com>
2024-07-04 16:30:13 -0300

    Inicia projeto
    projeto desenvolvido durante curso de java da udemy (prof. nelio alves).

---
+++ src/main/java/com/rudineidebrito18/github/curso_jpa_hibernate/CursoJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.rudineidebrito18.github.curso_jpa_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rudineidebrito18/github/curso_jpa_hibernate/CursoJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rudineidebrito18.github.curso_jpa_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
